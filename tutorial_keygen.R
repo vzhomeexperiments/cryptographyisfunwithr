5ab2f071ab493d5fc822310869d4d244e2ab1ad5
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-06-18 22:10:18 +0200

    added script for keys

---
+++ tutorial_keygen.R
@@ -0,0 +1,34 @@
+# R Script to Encrypt/Decrypt information
+# ONLY FOR TRAINING PURPOSES
+# UDEMY Course: "Cryptography is more fun with R!"
+# (C) 2018 Vladimir Zhbanko, vz.home.experiments@gmail.com
+# Enjoying the code? Join the course https://udemy.com/keep-secret-under-control
+
+### Used Libraries:
+# install.packages("openssl")
+# install.packages("tidyverse")
+library(openssl)
+library(tidyverse)
+
+#help ?`openssl-package`
+
+#### GENERATE RSA KEYS ####
+if(!exists("keys")){dir.create("keys")}
+# generate your private key (NB: make sure to do back up copy!!!)
+rsa_keygen(bits = 2099) %>% 
+  write_pem(path = "keys/private.pem", password = "")
+
+# generate your public key (NB: optional. Use Private Key to encrypt/decrypt)
+read_key(file = "keys/private.pem", password = "") %>% 
+  # extract element of the list and write to file
+  `[[`("pubkey") %>% write_pem("keys/public.pub")
+
+#### UNDERSTAND RSA KEYS ####
+# Note: this procedure is only for understanding
+# read private key from file and output structure of the object
+private_key <- read_key("keys/private.pem", password = "")
+# look about the structure of the key
+str(private_key)
+
+# delete history and empty environment
+rm(private_key)
